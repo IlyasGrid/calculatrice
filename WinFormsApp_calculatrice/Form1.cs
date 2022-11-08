d44254d8ef208ac4d9f84cfdf27e3004c8c99c2c
Admin <Admin@DESKTOP-IOMF4D2>
2022-11-08 10:22:15 +0100

    first try

--- WinFormsApp_calculatrice/Form1.cs
+++ WinFormsApp_calculatrice/Form1.cs
@@ -6,90 +6,113 @@ namespace WinFormsApp_calculatrice
         {
             InitializeComponent();
         }
+        string collector = "";
+        string firstOP = "";
+        string secondOP = "";
+        string theOperator = "";
+        string operation = "";
 
-     
-      private void BtnZERO_Click(object sender, EventArgs e)
+        int firstOPnumeric;
+        int secondOPnumeric;
+        int resultat ;
+
+
+        private void BtnZERO_Click(object sender, EventArgs e)
         {
-
+            collector += "0";
         }             
 
         private void BtnUN_Click(object sender, EventArgs e)
         {
-
+            collector += "1";
         }
        private void BtnDEUX_Click(object sender, EventArgs e)
         {
-
+            collector += "2";
         }
       
         private void BtnTROIS_Click(object sender, EventArgs e)
         {
-
+            collector += "3";
         }
 
         private void BtnQUATRE_Click(object sender, EventArgs e)
         {
-
+            collector += "4";
         }
 
         private void BtnCINQ_Click(object sender, EventArgs e)
         {
-
+            collector += "5";
         }
 
         private void BtnSIX_Click(object sender, EventArgs e)
         {
-
+            collector += "6";
         }
 
         private void BtnSEPT_Click(object sender, EventArgs e)
         {
-
+            collector += "7";
         }
 
         private void BtnHUIT_Click(object sender, EventArgs e)
         {
-
+            collector += "8";
         }
 
         private void BtnNEUF_Click(object sender, EventArgs e)
         {
-
+            collector += "9";
         }
 
         private void BtnEGALE_Click(object sender, EventArgs e)
         {
-
+            secondOP = collector;
+            collector = "";
+            operation += secondOP ;
         }
 
         private void BtnPLUS_Click(object sender, EventArgs e)
         {
-
+            firstOP = collector;
+            collector = "";
+            theOperator = "+";
+            operation = firstOP + theOperator; 
         }
 
         private void BtnMOIN_Click(object sender, EventArgs e)
         {
-
+            firstOP = collector;
+            collector = "";
+            theOperator = "-";
+            operation = firstOP + theOperator;
         }
 
         private void BtnMULTIPLIE_Click(object sender, EventArgs e)
         {
-
+            firstOP = collector;
+            collector = "";
+            theOperator = "*";
+            operation = firstOP + theOperator;
         }
 
         private void BtnDIVISE_Click(object sender, EventArgs e)
         {
-
+            firstOP = collector;
+            collector = "";
+            theOperator = "/";
+            operation = firstOP + theOperator;
         }
 
         private void LblRESULTAT_Click(object sender, EventArgs e)
         {
-
+            LblRESULTAT.Text = "resultat";
         }
 
         private void LblOPERATION_Click(object sender, EventArgs e)
         {
-
+            LblOPERATION.Text = collector;
         }
 
   
